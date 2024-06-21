a3eedccf2f4ac47371d59aa3029a681f7856392c
bomba-cat <bledion.morina@hotmail.com>
2024-06-21 18:52:03 +0200

    added nvim support

--- install.sh
+++ install.sh
@@ -9,11 +9,22 @@ read -p "Are you sure you want to continue? CTRL+C to cancel" silly
 rm $HOME/.vimrc
 cp ./.vimrc $HOME/.vimrc
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+read -p "Are you also using Neovim? [y/n] " type
+
+if [[ $type == "y" ]] then
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    rm $HOME/.config/nvim/init.vim
+cp ./init.vim $HOME/.config/nvim/init.vim
+fi
+if [[ $type == "n" ]] then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
 echo
-echo Done! Please run 'vim' inside your terminal and run ':PlugInstall' inside vim
+echo Done! Please run 'vim' or 'nvim' inside your terminal and run ':PlugInstall'
 echo
 read -p "Do you want to see some basic shortcuts? CTRL+C to cancel" silly
 ./tutorial.sh
